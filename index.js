b5881bec3634a79598effee0e79a6fb7c4187b4e
koko37 <engel0088@gmail.com>
2026-03-17 21:20:52 +0800

    use randomPassphrase, randomPin from core package

--- index.js
+++ index.js
@@ -4,18 +4,19 @@ import {
   getEvmWalletAddress,
   getTronWalletAddress,
   generateToken,
+  randomPassphrase,
+  randomPIN,
 } from "@mybucks.online/core";
-import { generatePassphrase, generatePIN } from "./lib.js";
 
 async function main() {
   const count = parseInt(process.argv[2]) || 1;
   const network = process.argv[3] || "polygon";
 
   for (let i = 0; i < count; i++) {
-    const passphrase = generatePassphrase(6);
-    const pin = generatePIN();
+    const passphrase = randomPassphrase(6);
+    const pin = randomPIN(8);
     const hash = await generateHash(passphrase, pin);
-    const privateKey = getEvmPrivateKey(hash);
+    // const privateKey = getEvmPrivateKey(hash);
     const address =
       network === "tron"
         ? getTronWalletAddress(hash)
